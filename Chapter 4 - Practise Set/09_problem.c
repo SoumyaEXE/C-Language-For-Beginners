7990b7cca7523654319e9a86df3e19a5bb50ab55
Soumyadeep Dey <soulsoumya1234@gmail.com>
2025-09-22 22:27:18 +0530

    Prime Number & Factorial Program.

---
+++ Chapter 4 - Practise Set/09_problem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int product = 1;
+    int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        product *= i;
+    }
+    printf("The factorial Is %d", product);
+    
+    return 0;
+}
---
+++ Chapter 4 - Practise Set/11_problem.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int prime = 1;
+    int n = 6;
+    for (int i = 2; i < n; i++)
+    {
+        if(n%1 == 0){
+            prime = 0;
+            break;
+        }
+    }
+
+    if(prime){
+        printf("%d is prime\n", n);
+    } else {
+        printf("%d is not prime\n", n);
+    }
+    
+    return 0;
+}
