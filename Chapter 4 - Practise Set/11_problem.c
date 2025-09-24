1e1a6244408bf300a1e0c7be9ef90797ef02c6e7
Soumyadeep Dey <soulsoumya1234@gmail.com>
2025-09-24 06:55:31 +0530

    Fixed Some Errors.

--- Chapter 4 - Practise Set/11_problem.c
+++ Chapter 4 - Practise Set/11_problem.c
@@ -2,19 +2,19 @@
 
 int main(){
     int prime = 1;
-    int n = 6;
+    int n = 13;
     for (int i = 2; i < n; i++)
     {
         if(n%1 == 0){
-            prime = 0;
+            prime = 0;       
             break;
         }
     }
 
     if(prime){
-        printf("%d is prime\n", n);
-    } else {
         printf("%d is not prime\n", n);
+    } else {
+        printf("%d is prime\n", n);
     }
     
     return 0;
